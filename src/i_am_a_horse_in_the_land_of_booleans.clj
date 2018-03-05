9f94221f433470bde9ad55168c917782de76add7
Mika <placeholder@notreal.email>
2018-03-05 21:59:45 +0200

    second one

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,57 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn
+  boolean
+  [x]
+  (if (= x false) false (if (= x nil) false true)))
 
-(defn abs [x]
-  ":(")
 
-(defn divides? [divisor n]
-  ":(")
 
-(defn fizzbuzz [n]
-  ":(")
+(defn
+  abs
+  [x]
+  (if (< x 0) (* -1 x) x))
 
-(defn teen? [age]
-  ":(")
+(defn
+  divides?
+  [divisor n]
+  (if (== (mod n divisor) 0) true false))
 
-(defn not-teen? [age]
-  ":(")
+(defn
+  fizzbuzz
+  [n]
+  (cond
+    (= 0 (mod n 15)) "gotcha!"
+    (= 0 (mod n 3)) "fizz"
+    (= 0 (mod n 5)) "buzz"
+    :else ""))
 
-(defn generic-doublificate [x]
-  ":(")
+(defn
+  teen?
+  [age]
+  (if (<= 13 age 19) true false))
 
-(defn leap-year? [year]
-  ":(")
+(defn
+  not-teen?
+  [age]
+  (if (teen? age) false true))
 
+(defn
+  generic-doublificate
+  [x]
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (or (list? x) (vector? x)) (* 2 (count x))
+    :else true))
+
+(defn
+  leap-year?
+  [year]
+  (cond
+    (= 0 (mod year 400)) true
+    (= 0 (mod year 100)) false
+    (= 0 (mod year 4)) true
+    :else false))
 ; '_______'
